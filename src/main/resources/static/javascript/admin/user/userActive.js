25c8d1a710962fc745b9bdbd131ac0878f1df381
Salva <salvaromanes@uma.es>
2024-08-26 18:04:26 +0200

    El bloque y desbloqueo ya funciona a la perfeccion

--- src/main/resources/static/javascript/admin/user/userActive.js
+++ src/main/resources/static/javascript/admin/user/userActive.js
@@ -25,15 +25,30 @@ function mapearUsuario(usuario){
             <td>${usuario.email}</td>
             <td>${usuario.municipio}</td>
             <td>
-            <button type="button" class="btn btn-primary">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-exclamation" viewBox="0 0 16 16">
-                  <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
-                  <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0m-3.5-2a.5.5 0 0 0-.5.5v1.5a.5.5 0 0 0 1 0V11a.5.5 0 0 0-.5-.5m0 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1"/>
-                </svg>
-                <a href="http://localhost:8080/api/user/blockUser/${usuario.dni}" class="btn btn-primary">Bloquear</a>
-            </button>
+                <button type="button" class="btn btn-primary" onclick="bloquearUsuario('${usuario.dni}')">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill-dash" viewBox="0 0 16 16">
+                      <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7M11 12h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1 0-1m0-7a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+                      <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+                    </svg>
+                </button>
             </td>
         </tr>`;
+}
+
+function bloquearUsuario(dni) {
+    fetch('http://localhost:8080/api/user/blockUser/' + dni, {
+        method: 'GET',
+    })
+    .then(response => {
+        if (response.ok) {
+            location.reload();
+        } else {
+            alert('Error al bloquear al usuario');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 }
 
 $(document).ready(function() {
